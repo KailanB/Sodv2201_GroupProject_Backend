cd0444d1f4e6da977c6a4c3d28e3fb5702458b5c
KailanB <kailanbates@gmail.com>
2024-10-26 18:37:12 -0600

    Initial file structure

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const port = 5000;
+
+
+app.use(express.json());
+app.get('/', (req, res) => {
+ res.send('Hello, Class!');
+});
+
+
+app.listen(port, () => {
+ console.log(`Server is running at http://localhost:${port}`);
+});
