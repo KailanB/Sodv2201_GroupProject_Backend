8b208024a48d0ef37998c6f1c5b19d7f515004fa
kajal-958 <163466004+kajal-958@users.noreply.github.com>
2024-11-20 13:16:57 -0700

    updated the router and import

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getCourses, getCoursesOfProgram, getCoursesOfUser, createCourse, updateCourse, deleteCourse } from '../controllers/courseController.js';
+import { authenticateToken } from '../controllers/authMiddleware.js';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get('/courses/:id', getCoursesOfUser);
 router.get('/courses/program/:programId', getCoursesOfProgram);
 router.put('/courses/:id', updateCourse);
 router.delete('/courses/:id', deleteCourse);
+router.post('/create-course', authenticateToken, createCourse);
 
 export default router;
