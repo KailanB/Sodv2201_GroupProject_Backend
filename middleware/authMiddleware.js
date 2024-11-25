9ff529e0c021d6a04c4234e08e34b1ecab3bc0fc
KailanB <kailanbates@gmail.com>
2024-11-24 21:55:16 -0700

    was trying to get the token route working but coulnd't

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,11 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
 
 // Middleware to authenticate the token and protect routes
 export const authenticateToken = (req, res, next) => {
+
     const token = req.header('Authorization')?.split(' ')[1];  // Get the token from Authorization header
+    console.log(token);
+    // this was how the example in class pulled the token
+    // const token = req.cookies.token;
 
     if (!token) {
         return res.status(401).json({ error: 'Access denied, no token provided' });
@@ -17,6 +21,7 @@ export const authenticateToken = (req, res, next) => {
         if (err) {
             return res.status(403).json({ error: 'Invalid or expired token' });
         }
+        
         req.user = user;  
         next();  
     });
