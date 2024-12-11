f3c14e30e70e00841b12be36f928c1b5866fab37
kajal-958 <163466004+kajal-958@users.noreply.github.com>
2024-12-11 16:50:00 -0700

    chnages

--- app.js
+++ app.js
@@ -23,6 +23,19 @@ dotenv.config();
 
 const app = express();
 const port = 5000;
+import express from 'express';
+import cors from 'cors';
+
+
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true // if you need to send cookies
+}));
+
+// other middleware and routes
+// ...
 
 // Middleware
 app.use(express.json());
